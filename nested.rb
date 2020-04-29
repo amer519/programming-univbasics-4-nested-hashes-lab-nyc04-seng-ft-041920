40cf68a62549fa871c771a1124a6afc79c409c76
Amer Fahmy <afahmy519@gmail.com>
2020-04-29 19:33:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-
+:yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+ }
 # return the entire updated hash
 
 	programmer_hash = 
