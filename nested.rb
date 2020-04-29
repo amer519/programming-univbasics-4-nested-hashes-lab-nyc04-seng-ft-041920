85dba76b0cf9d0539f8c15bec2379af45317856f
Amer Fahmy <afahmy519@gmail.com>
2020-04-29 18:53:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,11 +15,12 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:alan_kay][:known_for] =
+	programmer_hash[:alan_kay][:known_for] 
 	
 	programmer_hash = 
  		{
